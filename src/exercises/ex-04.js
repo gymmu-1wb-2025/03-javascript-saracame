b2e0751e74afad2bde35ef434991b79b3fafdcd7
Sara Camenzind <e273618@sbl.ch>
2026-01-28 11:11:15 +0100

    Aufgabe 04 Zahlen eingefügt

--- src/exercises/ex-04.js
+++ src/exercises/ex-04.js
@@ -1,10 +1,10 @@
-let weight = Number(process.argv[2]);
-let heightCm = Number(process.argv[3]);
+let weight = 55;
+let heightCm = 165;
 
 let heightM = heightCm / 100;
 
 let bmi = weight / (heightM * heightM);
 
 console.log("Gewicht: " + weight + " kg");
-console.log("Körpergrösse: " + heightCm + " cm");
+console.log("Körpergröße: " + heightCm + " cm");
 console.log("BMI: " + bmi.toFixed(2));
